777fc6e9ffc1f64ce692d9aef140e76a2d55edf7
M0dii <53468997+m0diis@users.noreply.github.com>
2021-09-12 20:11:10 +0300

    Delete src/main/java/me/M0dii/OnlinePlayersGUI directory

---
+++ src/main/java/me/m0dii/onlineplayersgui/utils/UpdateChecker.java
@@ -0,0 +1,32 @@
+package me.m0dii.onlineplayersgui.utils;
+
+import me.m0dii.onlineplayersgui.OnlineGUI;
+import org.bukkit.Bukkit;
+import org.bukkit.util.Consumer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Scanner;
+
+public record UpdateChecker(OnlineGUI plugin, int resourceId)
+{
+    public void getVersion(final Consumer<String> consumer)
+    {
+        Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> {
+            try(InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId).openStream();
+
+                Scanner scanner = new Scanner(inputStream))
+            {
+                if(scanner.hasNext())
+                {
+                    consumer.accept(scanner.next());
+                }
+            }
+            catch(IOException ex)
+            {
+                this.plugin.getLogger().info("Failed to check for updates: " + ex.getMessage());
+            }
+        });
+    }
+}
